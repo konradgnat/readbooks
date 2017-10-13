68a5b057680780aae74cfd0318cbaaee79872c33
konradgnat <konradmgnat@gmail.com>
2017-10-12 23:03:51 -0500

    Add new seed file

--- seeds.js
+++ seeds.js
@@ -3,38 +3,30 @@ var mongoose = require('mongoose'),
 	Comment	= require('./models/comment');
 
 var data = [
-	{
-		title:"Eloquent Javascript",
-		author: "Marijn Haverbeke",
-		dateRead: Date.now(),
-		description: "This thoroughly revised edition reflects the current state of JavaScript and Web browsers, with new material, such as a chapter on code performance in JavaScript, and expanded coverage of recursion and closures. ...",
-		note: "super good",
-		postedBy: {
-			id: "59344d3e65f3a72f9ab06fc9",
-			email: "konrad9"
-		}
-	},
-	{
-		title:"The Chronicles of Narnia",
-		author: "C. S. Lewis",
-		dateRead: Date.now(),
-		description: "The Chronicles of Narnia is a series of seven fantasy novels by C. S. Lewis. It is considered a classic of children's literature and is the author's best-known work, having sold over 100 million copies in 47 languages....",
-		note: "so so ",
-		postedBy: {
-			id: "59344d3e65f3a72f9ab06fc9",
-			email: "konrad2"
-		}
-	},
-	{
-		title:"Twenty Years at Hull House",
-		author: "Jane Addams",
-		dateRead: Date.now(),
-		description: "Twenty Years at Hull House is the story of the Hull House settlement by its founder Jane Addams. The settlement movement, which gained popularity first in London at the end of the 19th century, soon ......",
-		note: "super good",
-		postedBy: {
-			id: "59344d3e65f3a72f9ab06fc9",
-			email: "konrad"
-		}
+	{ "_id" : ("59e034d6b6d54d54eb3e0bd9"), 
+	"thumbnail" : "http://books.google.com/books/content?id=sQYqRCIhFAMC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api", 
+	"title" : "The Power of Now", "author" : "Eckhart Tolle", "publishedDate" : "2010-10-06", 
+	"description" : "To make the journey into the Now we will need to leave our analytical mind and its false created self, the ego, behind. From the very first page of Eckhart Tolle's extraordinary book, we move rapidly into a significantly higher altitude where we breathe a lighter air. We become connected to the indestructible essence of our Being, “The eternal, ever present One Life beyond the myriad forms of life that are subject to birth and death.” Although the journey is challenging, Eckhart Tolle uses simple language and an easy question and answer format to guide us. A word of mouth phenomenon since its first publication, The Power of Now is one of those rare books with the power to create an experience in readers, one that can radically change their lives for the better.", "thoughts" : "I read this ten times. ", "comments" : [ ("59e034dfb6d54d54eb3e0bda"), 
+	("59e034eab6d54d54eb3e0bdb") ], 
+	"postedBy" : { "id" : ("59e033bab6d54d54eb3e0bd4"), 
+	"email" : "asfe" }, "__v" : 2 
+},
+	{ "_id" : ("59e03445b6d54d54eb3e0bd6"), 
+	"thumbnail" : "http://books.google.com/books/content?id=vxbUjwEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api", 
+	"title" : "Secrets of the JavaScript Ninja", "author" : "John Resig", "publishedDate" : "2016-03", 
+	"description" : "More than ever, the web is a universal platform for all types of applications, and JavaScript is the language of the web. For anyone serious about web development, it's not enough to be a decent JavaScript coder. They need to be ninja-stealthy, efficient, and ready for anything. Secrets of the JavaScript Ninja, Second Edition dives below the surface and helps readers understand the deceptively-complex world of JavaScript and browser-based application development. It skips the basics, and dives into core JavaScript concepts such as functions, closures, objects, prototypes, promises, and so on. With examples, illustrations, and insightful explanations, readers will benefit from the collective wisdom of seasoned experts John Resig, Bear Bibeault, and Josip Maras. Purchase of the print book includes a free eBook in PDF, Kindle, and ePub formats from Manning Publications.", "thoughts" : "I swear by this book!", 
+	"comments" : [ ("59e0345bb6d54d54eb3e0bd7"), ("59e03470b6d54d54eb3e0bd8") ], "postedBy" : { "id" : ("59e033bab6d54d54eb3e0bd4"), "email" : "asfe" }, "__v" : 2 
+},{ 
+		"_id" : ("59e033d7b6d54d54eb3e0bd5"), 
+		"thumbnail" : "http://books.google.com/books/content?id=ZEQvvmF7ZrwC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api", 
+		"title" : "The Odyssey of Homer", 
+		"author" : "Butcher,S.H..", 
+		"publishedDate" : "2000", 
+		"description" : "", 
+		"thoughts" : "This is my favorite book of all time!", 
+		"comments" : [ ], "postedBy" : { "id" : ("59e033bab6d54d54eb3e0bd4"), 
+		"email" : "asfe" }, 
+		"__v" : 0 
 	}
 ]
 
