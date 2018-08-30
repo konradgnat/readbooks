2a33d1846f70ec71e323e63a0bc37e46aea15143
konradgnat <konradmgnat@gmail.com>
2018-08-30 07:31:07 -0500

    tab

--- app/containers/Profile.js
+++ app/containers/Profile.js
@@ -1,11 +1,45 @@
 import * as React from 'react';
-import Tab from '../components/Tab';
+import PostsTab from '../components/PostsTab';
+
+export const tabs = [
+  {id: 'posts', label: 'Posts', active: true, content: <PostsTab/>},
+  {id: 'readingList', label: 'Reading List', active: false, content: <PostsTab/>},
+  {id: 'following', label: 'Following', active: false, content: <PostsTab/>},
+  {id: 'followers', label: 'Followers', active: false, content: <PostsTab/>},
+];
 
 export default class Profile extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      tabs: tabs,
+      activeTab: 'posts'
+    }
+  }
+
+  changeTab = (tabId) => {
+    let updatedTab = this.state.tabs.map(t => {
+      Object.assign({}, t, {active: t.id === tabId})
+    });
+    this.setState({
+      tabs: updatedTab
+    })
+  };
 
   render() {
-    const user = this.props.appData;
-    const avatar = user.avatar ? '/' + user.avatar : '/images/avatar-placeholder.jpg';
+    const user = this.props.appData,
+      avatar = user.avatar ? '/' + user.avatar : '/images/avatar-placeholder.jpg',
+      tabButtons = [],
+      tabContent = [];
+    tabs.forEach((tab) => {
+      let active = tab.active ? 'active' : '';
+      tabButtons.push(
+        <a key={tab.id} className={`item ${active}`} data-tab={tab.id} onClick={() => this.changeTab(tab.id)}>{tab.label}</a>
+      );
+      tabContent.push(
+        <div key={tab.id} className={`ui bottom attached tab segment ${active}`}>{tab.content}</div>
+      )
+    });
     return (
       <div className="segment">
         <a href="/" className="ui right floated mini primary button basic">Edit</a>
@@ -32,21 +66,9 @@ export default class Profile extends React.Component<Props> {
         <div className="ui blue divider"></div>
 
         <div className="ui top attached tabular menu">
-          <a className="item" data-tab="first">Posts</a>
-          <a className="item" data-tab="second">Reading List</a>
-          <a className="item" data-tab="third">Following</a>
-          <a className="item active" data-tab="third">Followers</a>
+          {tabButtons}
         </div>
-        <div className="ui bottom attached tab segment" data-tab="first">
-          First
-        </div>
-        <div className="ui bottom attached tab segment" data-tab="second">
-          Second
-        </div>
-        <div className="ui bottom attached tab segment active" data-tab="third">
-          Third
-        </div>
-        <Tab />
+        {tabContent}
       </div>
     )
   }
